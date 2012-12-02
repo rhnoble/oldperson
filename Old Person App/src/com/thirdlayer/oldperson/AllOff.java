8e2a040a0afa84de1b96cc8775f37b9a92bffdfb
Harry Noble <rhnoble@gmail.com>
2012-12-01 16:54:57 -0800

    Added blank buttons to all_off layout.

--- Old Person App/src/com/thirdlayer/oldperson/AllOff.java
+++ Old Person App/src/com/thirdlayer/oldperson/AllOff.java
@@ -1,8 +1,13 @@
+
 package com.thirdlayer.oldperson;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class AllOff extends Activity {
 
@@ -10,6 +15,30 @@ public class AllOff extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.all_off);
+
+        Button mButtonMagnify = (Button) findViewById(R.id.magnify);
+        Button mButtonNotes = (Button) findViewById(R.id.notes);
+        Button mButtonLight = (Button) findViewById(R.id.light);
+
+        mButtonMagnify.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                Intent mGoToMagnify = new Intent(AllOff.this, Maginfy.class);
+                AllOff.this.startActivity(mGoToMagnify);
+            }
+        });
+        
+        mButtonNotes.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                Intent mGoToMagnify = new Intent(AllOff.this, Notes.class);
+                AllOff.this.startActivity(mGoToMagnify);
+            }
+        });
+        
+        mButtonNotes.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+            }
+        });
+
     }
 
     @Override
@@ -17,4 +46,5 @@ public class AllOff extends Activity {
         getMenuInflater().inflate(R.menu.all_off, menu);
         return true;
     }
+
 }
